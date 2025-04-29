f44f31abbdac1a0dc3f8da84742fab4df6aa8b4d
Poueplo <104569053+Poueplo@users.noreply.github.com>
2025-04-29 12:39:01 +0200

    warning removal

--- hal/lib/pwm/src/lib.rs
+++ hal/lib/pwm/src/lib.rs
@@ -38,6 +38,8 @@
 ****************************************************************************/
 
 #![no_std]
+#![allow(non_camel_case_types)]
+#![allow(non_upper_case_globals)]
 
 use hal_ll_tim::*;
 use hal_target::*;
